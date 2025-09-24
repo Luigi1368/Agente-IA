72c2bc67b89aafc2a24bd8bc4171fa4cfa9da441
Luigi <ra50242081886@acaonsfatima.org.br>
2025-09-24 17:10:07 -0300

    popo

--- backend/api.js
+++ backend/api.js
@@ -1,4 +1,8 @@
 import express from 'express'
 import cors from 'cors'
 const api = express();
+
 api.use(express.json());
+api.use(cors());
+
+api.listen(7483,() => console.log('==> > SUPER API em funcionamento!!!'));
