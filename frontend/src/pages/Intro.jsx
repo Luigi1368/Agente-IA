d5172ea220e3658f2083afda00fbff392803072b
MaiaDiber <>
2025-09-24 17:09:52 -0300

    hcghc

--- frontend/src/pages/Intro.jsx
+++ frontend/src/pages/Intro.jsx
@@ -1,10 +1,17 @@
 import './intro.scss'
+import { Link } from 'react-router'
 
 export default function Intro () {
     return(
 
-        <div className="Pai">
-            <h1>Intrão</h1>
-        </div>
+       <>
+       <div className="background-animation"></div>
+
+    <div className="intro-container">
+    <h1>Bem-vindo ao Nosso Site</h1>
+    <p>Descubra experiências incríveis clicando abaixo</p>
+    <Link to={'/Agt'} className='enter-button' > Cagado </Link>
+    </div>
+       </>
     )
 }
