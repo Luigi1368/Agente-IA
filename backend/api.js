13883e5e3a16f6339d62465d1171f553555ea015
Luigi <ra50242081886@acaonsfatima.org.br>
2025-09-24 18:11:12 -0300

    per

--- backend/api.js
+++ backend/api.js
@@ -1,7 +1,8 @@
 import express from 'express'
 import cors from 'cors'
-const api = express();
 
+
+const api = express();
 api.use(express.json());
 api.use(cors());
 
---
+++ backend/perguntas.js
@@ -0,0 +1,62 @@
+export const perguntas = [
+  {
+    id: 1,
+    texto: "Qual atividade você mais gosta?",
+    alternativas: { 
+      A: "Ensinar", 
+      B: "Limpar ruas", 
+      C: "Atender pacientes", 
+      D: "Programar softwares" 
+    }
+  },
+  {
+    id: 2,
+    texto: "Qual tarefa você prefere?",
+    alternativas: { 
+      A: "Criar artes", 
+      B: "Consertar máquinas", 
+      C: "Ensinar alunos", 
+      D: "Atender pacientes" 
+    }
+  },
+  {
+    id: 3,
+    texto: "Qual ambiente de trabalho você gosta mais?",
+    alternativas: { 
+      A: "Sala de aula", 
+      B: "Rua", 
+      C: "Hospital", 
+      D: "Escritório de TI" 
+    }
+  },
+  {
+    id: 4,
+    texto: "Escolha uma habilidade que você prefere usar:",
+    alternativas: { 
+      A: "Criatividade", 
+      B: "Lógica", 
+      C: "Força", 
+      D: "Conhecimento técnico" 
+    }
+  },
+  {
+    id: 5,
+    texto: "Com o que você mais se identifica?",
+    alternativas: { 
+      A: "Ajudar pessoas", 
+      B: "Ensinar", 
+      C: "Criar soluções digitais", 
+      D: "Manutenção de equipamentos" 
+    }
+  },
+  {
+    id: 6,
+    texto: "O que mais te motiva?",
+    alternativas: { 
+      A: "Transformar espaços ", 
+      B: "Servir à comunidade", 
+      C: "Salvar vidas", 
+      D: "Resolver problemas técnicos" 
+    }
+  }
+];
