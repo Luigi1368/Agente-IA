5b27b450599adcd3ea1910c555ee1ed9ab47fd05
MaiaDiber <gustavomaiaxre@gmail.com>
2025-09-25 00:57:36 -0300

    atualizando as duas págs

--- frontend/src/pages/Intro.jsx
+++ frontend/src/pages/Intro.jsx
@@ -5,13 +5,15 @@ export default function Intro () {
     return(
 
        <>
+       <div className="intro-page">
        <div className="background-animation"></div>
 
-    <div className="intro-container">
-    <h1>Bem-vindo ao Nosso Site</h1>
-    <p>Descubra experiências incríveis clicando abaixo</p>
-    <Link to={'/Agt'} className='enter-button' > Cagado </Link>
-    </div>
+<div className="intro-container">
+<h1>Bem-vindo ao agente de IA</h1>
+<p>Descubra sua profissão</p>
+<Link to={'/Agt'} className='enter-button' > Iniciar </Link>
+</div>
+       </div>
        </>
     )
 }
--- frontend/src/pages/agent.jsx
+++ frontend/src/pages/agent.jsx
@@ -3,8 +3,30 @@ import './agentStyle.scss'
 
 export default function Agente() {
     return (
-        <div className="Container">
-            <h1>sdbhfksbkbfsb</h1>
-        </div>
+        <section className="Container">
+            <div className="formul">
+            <h1>Conclua abaixo</h1>
+            </div>
+
+            <div className="main">
+                <h1>Escolha</h1>
+                <label htmlFor="">
+                <input type="radio" />
+                A
+                </label>
+                <label htmlFor="">
+                <input type="radio" />
+                B
+                </label>
+                <label htmlFor="">
+                <input type="radio" />
+                C
+                </label>
+               <label htmlFor="">
+               <input type="radio" />
+               D
+               </label>
+            </div>
+        </section>
     )
 }
