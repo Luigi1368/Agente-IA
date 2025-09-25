d70f146fdda653d82ad3b598458420c3b4df7da2
Luigi <ra50242081886@acaonsfatima.org.br>
2025-09-25 18:01:17 -0300

    prontp

--- backend/api.js
+++ backend/api.js
@@ -1,9 +1,46 @@
 import express from 'express'
 import cors from 'cors'
-
-
+import { perguntas } from './perguntas.js'
+ 
 const api = express();
 api.use(express.json());
 api.use(cors());
+
+let respostas = {};
+
+api.post('/respostas', (req, resp) => {
+    const { userId, perguntaId, alternativa } = req.body;
+
+    if (!respostas[userId]) respostas[userId] = [];
+
+    const pergunta = perguntas[perguntaId];
+    const profissoes = pergunta.alternativas[alternativa];
+
+    respostas[userId].push(profissoes);
+
+    if (perguntaId < perguntas.length) {
+    const proximaPergunta = perguntas[perguntaId];
+    resp.json({ tipo: "pergunta", conteudo: proximaPergunta });
+    }
+
+    else {
+    const pontos = {};
+    respostas[userId].forEach(point => {
+      pontos[point] = (pontos[point] || 0) + 1;
+    });
+    }
+
+    let resultado = null;
+    let maior = 0;
+
+    for (const profissao in pontos) {
+      if (pontos[profissao] > maior) {
+        maior = pontos[profissao];
+        resultado = profissao;
+      }
+    }
+
+    resp.json({ tipo: "resultado", conteudo: `Sua profissão ideal é: ${resultado}` });
+});
 
 api.listen(7483,() => console.log('==> > SUPER API em funcionamento!!!'));
